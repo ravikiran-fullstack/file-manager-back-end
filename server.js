cc1c12220a1764ad04b00e3a431a8c7d0accbd4e
kiran-code <ravikiransjce.code@gmail.com>
2020-12-30 00:10:59 +0530

    Updated code to allow cross origin request

--- server.js
+++ server.js
@@ -5,16 +5,9 @@ const path = require('path');
 const server = http.createServer( async (req, res) => {
   const url = req.url;
   const method = req.method;
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
+  
+  if (url === '/fm' && method === 'POST') {
     const body = [];
-    let filesObjArr = [];
     req.on('data', (chunk) => {
       console.log(chunk);
       body.push(chunk);
@@ -24,31 +17,21 @@ const server = http.createServer( async (req, res) => {
       const message = parsedBody.split('=')[1];
       fs.appendFileSync('message.txt', message);        
     });
-   // res.statusCode = 302;
-   // res.setHeader('Location', '/');
+   
    const data = await readDirectory();
-   console.log(req.url);
    console.log(data);
+   res.setHeader("Access-Control-Allow-Origin", "*");
    res.setHeader('Content-Type', 'application/json');
-   return res.end(JSON.stringify(data));
+   
+   return res.end(JSON.stringify({filesInfo: data, path: __dirname}));
   }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
 });
 
 server.listen(3000);
 
 function readDirectory() {
   return new Promise(resolve => {
-      // setTimeout(() => {
-      //     resolve({data: 1});
-      // }, 1000);
-
-      const fileNames = fs.readdirSync(__dirname);
+    const fileNames = fs.readdirSync(__dirname);
 
     filesObjArr = fileNames.map((file) => {
       let fileObj = {};
@@ -57,9 +40,6 @@ function readDirectory() {
         path.extname(file) === "" ? "directory" : path.extname(file);
       return fileObj;
     });
-
-    console.log("after", fileNames);
-    console.log("after", filesObjArr);
     resolve(filesObjArr);
   });
 }
